ec0d0d89476d4c3998bfb250c0fa49c0e995506a
Cadel Watson <cadel@cadelwatson.com>
2017-09-15 18:30:54 +1000

    Create percentage input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      examMark: 0,
+      examWeighting: 0,
+      examHurdle: 0,
+      assessments: []
+    };
+
+    this.examMarkChanged = this.examMarkChanged.bind(this);
+  }
+
+  examMarkChanged(value) {
+    this.setState({ examMark: value });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,11 +26,44 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.examMark}</p>
+        <PercentageInput handler={this.examMarkChanged} />
       </div>
     );
+  }
+}
+
+class PercentageInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0,
+      valid: true
+    };
+
+    this.edited = this.edited.bind(this);
+  }
+
+  render() {
+    return (
+      <input
+        onChange={this.edited}
+        className={this.state.valid ? "" : "error"}
+      />
+    );
+  }
+
+  edited(event) {
+    console.log("edited");
+    let value = parseInt(event.target.value);
+
+    if (isNaN(value) | (value > 100) | (value < 0)) {
+      this.setState({ valid: false });
+    } else {
+      this.setState({ value: value, valid: true });
+      this.props.handler(value);
+    }
   }
 }
 
